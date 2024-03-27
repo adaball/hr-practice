29478e377e5457797bae78ff44a16829da98cf4f
Adam Ball <etc@adamball.org>
2024-03-27 11:43:33 -0500

    2 more solutions

---
+++ src/main/java/org/adamball/introduction/EndOfFile.java
@@ -0,0 +1,30 @@
+package org.adamball.introduction;
+
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+import java.util.Scanner;
+import org.adamball.Util;
+
+/**
+ * Solution for: <a href="https://www.hackerrank.com/challenges/java-end-of-file/problem">HR link</a>
+ */
+public class EndOfFile {
+  public static void main(String[] args) {
+    InputStream inputStream;
+
+    try {
+      inputStream = Util.loadResourceAsStream("end-of-file.txt");
+    } catch (FileNotFoundException e) {
+      throw new RuntimeException(e);
+    }
+
+    Scanner scanner = new Scanner(inputStream);
+
+    int i = 1;
+    while (scanner.hasNext()) {
+      System.out.printf("%d %s%n", i++, scanner.nextLine());
+    }
+
+    scanner.close();
+  }
+}
---
+++ src/main/java/org/adamball/introduction/OutputFormatting.java
@@ -0,0 +1,34 @@
+package org.adamball.introduction;
+
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+import java.util.Scanner;
+import org.adamball.Util;
+
+/**
+ * Solution for: <a href="https://www.hackerrank.com/challenges/java-output-formatting/problem">HR link</a>
+ */
+public class OutputFormatting {
+  public static void main(String[] args) {
+    InputStream inputStream;
+
+    try {
+      inputStream = Util.loadResourceAsStream("output-formatting.txt");
+    } catch (FileNotFoundException e) {
+      throw new RuntimeException(e);
+    }
+
+    Scanner scanner = new Scanner(inputStream);
+
+    System.out.println("================================");
+    while (scanner.hasNext()) {
+      String s = scanner.next();
+      int i = scanner.nextInt();
+
+      System.out.printf("%-15s%03d%n", s, i);
+    }
+    System.out.println("================================");
+
+    scanner.close();
+  }
+}
