957b3b68085c128016279a40b8b8bf82cb87eb85
Ada Ball <etc@adamball.org>
2025-07-17 10:29:02 -0500

    another solution

---
+++ src/main/java/org/adaball/datastructures/HashSets.java
@@ -0,0 +1,51 @@
+package org.adaball.datastructures;
+
+import org.adaball.Util;
+
+import java.io.InputStream;
+import java.util.HashSet;
+import java.util.MissingResourceException;
+import java.util.Scanner;
+import java.util.Set;
+
+public class HashSets {
+    public static void main(String[] args) {
+        InputStream inputStream;
+        /**
+         * 5
+         * john tom
+         * john mary
+         * john tom
+         * mary anna
+         * mary anna
+         *
+         * (john, tom) -> 1 -> ((john, tom))
+         * (john, mary) -> 2 -> ((john, tom), (john, mary))
+         * (john, tom) -> 2 -> ((john, tom), (john, mary))
+         * (mary, anna) -> 3 -> ((john, tom), (john, mary), (mary, anna))
+         * (mary, anna) -> 3 -> ((john, tom), (john, mary), (mary, anna))
+         */
+
+        try {
+            inputStream = Util.loadResourceAsStream("datastructures/hashsets.txt");
+        } catch (MissingResourceException e) {
+            throw new RuntimeException(e);
+        }
+
+        Scanner scanner = new Scanner(inputStream);
+        int numLines = scanner.nextInt();
+        scanner.nextLine();
+
+        Set<Set<String>> pairs = new HashSet<>();
+        for (int i = 0; i < numLines; i++) {
+            String[] tokens = scanner.nextLine().split(" ");
+
+            Set<String> innerSet = new HashSet<>();
+            innerSet.add(tokens[0]);
+            innerSet.add(tokens[1]);
+            pairs.add(innerSet);
+
+            System.out.println(pairs.size());
+        }
+    }
+}
